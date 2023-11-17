b450c79b96831043fc35bca0032195f0d7bfee23
alreadycow <alreadycow_@naver.com>
2023-11-17 13:46:50 +0900

    검색 시 페이지 오류 수정

--- src/main/java/org/chajajo/service/ServiceService.java
+++ src/main/java/org/chajajo/service/ServiceService.java
@@ -6,11 +6,14 @@ import org.chajajo.domain.ServiceVO;
 import java.util.List;
 
 public interface ServiceService {
-
-    public List<ServiceVO> getList(Criteria cri);
-
 	public ServiceVO get(Long no);
-	
-	// pagination
+
+	/* 게시판 목록 */
+	public List<ServiceVO> getList(Criteria cri);
+
+	/* 게시판 목록 (페이징 적용) */
+	public List<ServiceVO> getListWithPaging(Criteria cri);
+
+	/* 게시판 총 갯수 */
 	public int getTotal(Criteria cri);
 }
